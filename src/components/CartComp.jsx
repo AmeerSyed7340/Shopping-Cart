18f3540b9f3a67b29a344f0ccdf776d783e1f298
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-04-29 15:16:17 -0400

    Updated cart component

--- src/components/CartComp.jsx
+++ src/components/CartComp.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Slide, Stack, Box, Button } from "@mui/material";
+import { Slide, Stack, Box, Button, Paper } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
@@ -12,7 +12,22 @@ import { useCart } from "../contexts/CartContext";
 import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 import { useState, useEffect } from "react";
+import { styled } from '@mui/material/styles';
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="left" ref={ref} {...props} />;
+});
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
+//component starts here
 const CartComp = ({ isOpen, setIsOpen }) => {
   const [totalPrice, setTotalPrice] = useState(0);
 
@@ -21,25 +36,25 @@ const CartComp = ({ isOpen, setIsOpen }) => {
 
   //immutable way to update the count
   const handleAdd = (productId) => {
-    // setProducts((currentProducts) =>
-    //   currentProducts.map((product) =>
-    //     product.id === productId
-    //       ? { ...product, count: product.count + 1 }
-    //       : product
-    //   )
-    // );
-    // setTotal((prevTotal) => prevTotal + 1);
+    setProducts((currentProducts) =>
+      currentProducts.map((product) =>
+        product.id === productId
+          ? { ...product, count: product.count + 1 }
+          : product
+      )
+    );
+    setTotal((prevTotal) => prevTotal + 1);
   };
 
   const handleRemove = (productId) => {
-    // setProducts((currentProducts) =>
-    //   currentProducts.map((product) =>
-    //     product.id === productId
-    //       ? { ...product, count: product.count - 1 }
-    //       : product
-    //   )
-    // );
-    // setTotal((prevTotal) => prevTotal - 1);
+    setProducts((currentProducts) =>
+      currentProducts.map((product) =>
+        product.id === productId
+          ? { ...product, count: product.count - 1 }
+          : product
+      )
+    );
+    setTotal((prevTotal) => prevTotal - 1);
   };
 
   const handleClickClose = () => {
@@ -54,9 +69,6 @@ const CartComp = ({ isOpen, setIsOpen }) => {
     setTotalPrice(newTotalPrice);
   }, [products]);
 
-  const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="left" ref={ref} {...props} />;
-  });
   return (
     <Dialog
       open={isOpen}
@@ -70,7 +82,7 @@ const CartComp = ({ isOpen, setIsOpen }) => {
         sx: {
           height: "100vh",
           maxHeight: "100vh",
-          minWidth: "500px",
+          minWidth: "600px",
           marginTop: "0px",
           marginLeft: "auto",
           marginBottom: "0px",
@@ -90,77 +102,88 @@ const CartComp = ({ isOpen, setIsOpen }) => {
           </IconButton>
         </DialogActions>
       </Stack>
+
       <DialogContent>
-        <Grid container justifyContent="center">
-          {products
-            .filter((product) => product.count > 0)
-            .map((product) => (
-              <Grid
-                item
-                key={product.id}
-                xs={12}
-                sx={{
-                  paddingTop: "0px",
-                  paddingBottom: "0px",
-                  paddingLeft: "0px",
-                  paddingRight: "0px",
-                }}
-              >
-                <Stack
-                  direction="row"
-                  spacing={2}
-                  justifyContent="space-evenly"
-                  alignItems="center"
+        {totalPrice === 0 ? <DialogContentText>Shopping bag is empty</DialogContentText> :
+          <Grid container justifyContent="center">
+            {products
+              .filter((product) => product.count > 0)
+              .map((product) => (
+                <Grid
+                  item
+                  key={product.id}
+                  xs={12}
+                  sx={{
+                    paddingTop: "0px",
+                    paddingBottom: "0px",
+                    paddingLeft: "0px",
+                    paddingRight: "0px",
+                  }}
                 >
-                  <Box
-                    component="img"
-                    sx={{
-                      height: 200,
-                      objectFit: "contain",
-                    }}
-                    src={product.image}
-                  ></Box>
                   <Stack
-                    direction="column"
+                    direction="row"
                     spacing={2}
-                    justifyContent="center"
-                    sx={{ width: "150px", margin: 0 }}
+                    justifyContent="space-evenly"
+                    alignItems="center"
                   >
-                    <DialogContentText>{product.title}</DialogContentText>
+                    <Box
+                      component="img"
+                      sx={{
+                        height: 200,
+                        objectFit: "contain",
+                      }}
+                      src={product.image}
+                    ></Box>
                     <Stack
-                      direction="row"
+                      direction="column"
                       spacing={2}
                       justifyContent="center"
-                      alignItems="center"
+                      sx={{ width: "150px", margin: 0 }}
                     >
-                      <Button
-                        variant="outlined"
-                        size="small"
-                        onClick={() => handleRemove(product.id)}
+                      <DialogContentText>{product.title}</DialogContentText>
+                      <Stack
+                        direction="row"
+                        spacing={2}
+                        justifyContent="center"
+                        alignItems="center"
                       >
-                        <RemoveIcon />
-                      </Button>
-                      <DialogContentText>{product.count}</DialogContentText>
-                      <Button
-                        variant="outlined"
-                        size="small"
-                        onClick={() => handleAdd(product.id)}
-                      >
-                        <AddIcon />
-                      </Button>
+                        <Button
+                          variant="outlined"
+                          size="small"
+                          onClick={() => handleRemove(product.id)}
+                        >
+                          <RemoveIcon />
+                        </Button>
+                        <DialogContentText>{product.count}</DialogContentText>
+                        <Button
+                          variant="outlined"
+                          size="small"
+                          onClick={() => handleAdd(product.id)}
+                        >
+                          <AddIcon />
+                        </Button>
+                      </Stack>
+                      <DialogContentText>
+                        {product.price * product.count}
+                      </DialogContentText>
                     </Stack>
-                    <DialogContentText>
-                      {product.price * product.count}
-                    </DialogContentText>
                   </Stack>
-                </Stack>
-              </Grid>
-            ))}
-          <DialogContentText sx={{ fontWeight: "bold", marginTop: "50px" }}>
-            Total: {totalPrice}
-          </DialogContentText>
-        </Grid>
+                </Grid>
+              ))}
+            {/* <Stack justifyContent="center" alignItems="center" sx={{background: "gray", marginTop: "50px", padding: "10px", height: "50px"}} > */}
+
+          </Grid>
+        }
       </DialogContent>
+      <Stack direction="row" justifyContent="space-evenly" alignItems="flex-end" sx={{ marginTop: "20px", paddingBottom: "20px", boxShadow: "0px 2px 4px 0px rgba(0, 0, 0, 0.5)" }}>
+        <DialogContentText sx={{ fontWeight: "bold", fontSize: "75px" }}>
+          ${totalPrice.toFixed(2)}
+        </DialogContentText>
+
+        <Button variant="contained" color="success" disabled={totalPrice === 0}>
+          CHECKOUT
+        </Button>
+      </Stack>
     </Dialog>
   );
 };
