b35bb2dde47271f0d29236066176bb50b850f18b
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-05-02 14:56:52 -0400

    Fixed videoComp route by prefixing it with Shopping-Cart

--- src/components/VideoComp.jsx
+++ src/components/VideoComp.jsx
@@ -10,7 +10,7 @@ const VideoComp = () => {
         style={{ width: "100%", height: "100%", objectFit: "cover"}}
       >
         <source
-          src="/src/assets/videos/3171546-hd_1920_1080_30fps.mp4"
+          src="/Shopiing-Cart/src/assets/videos/3171546-hd_1920_1080_30fps.mp4"
           type="video/mp4"
         />
         Your browser does not support the video tag.
